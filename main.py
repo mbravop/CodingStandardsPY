1186f3ccfe20973d9ec3766ebd264e7a1b2acd38
mbravop <mbravop03@gmail.com>
2024-05-29 14:52:30 -0500

    Final

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-class myclass:
+class MyClass:
     def __init__(self):
         self.destinies = {'Paris': 500, 'NYC': 600}
 
@@ -6,56 +6,29 @@ class myclass:
         return self.destinies.get(dist, 0)
 
     def valid_this(self, dist):
-        return type(dist).isInstance(str)
+        return isinstance(dist,str)
 
-class passanger:
+class Passenger:
     def __init__(self, num):
         self.num = num
 
     def valid_number(self):
-        print("this working here")
-        return type(self.num).isInstance(str) and self.num > 0
+        return isinstance(self.num, int) and self.num > 0
 
     def for_here_discount(self):
         if 4 < self.num < 10:
             return 0.1
         elif self.num >= 10:
             return 0.2
-        # TODO: add more discount levels if needed
         else:
             return 0.0
 
-
-class Plane:
-    def __init__(self, dist, num, dur):
-        self.myclass = myclass()
-        self.passanger = passanger(num)
-        self.total_time = total_time(dur)
-        self.dist = dist
-        self.seats = 200
-
-    def sum(self):
-        if not self.myclass.validThis(
-                self.dist) or not self.passanger.validNumber() or not self.total_.is_valid_total_time():
-            return -1
-
-        number_total = self.costBas
-        number_total += self.myclass.get_extraCost(self.dist)
-        number_total += self.total_time.getFee()
-        number_total -= self.total_time.getTheBestPromoEver()
-
-        discount = self.passanger.forHereDiscount()
-        number_total = number_total - (number_total * discount)
-
-        return max(int(number_total), 0)
-
-
-class total_time:
+class TotalTime:
     def __init__(self, dur):
         self.dur = dur
 
-    def is_valid_total_time(self,dur):
-        return type(dur).isInstance(int) and self.dur > 0
+    def is_valid_total_time(self):
+        return isinstance(self.dur, int) and self.dur > 0
 
     def get_fee(self):
         return 200 if self.dur < 7 else 0
@@ -69,36 +42,32 @@ class total_time:
 
 class Vacation:
     base_cost = 1000
-
     def __init__(self, dist, num, dur):
-        self.myclass = myclass()
-        self.passanger = passanger(num)
-        self.total_time = total_time(dur)
+        self.myclass = MyClass()
+        self.passenger = Passenger(num)
+        self.total_time = TotalTime(dur)
         self.dist = dist
         
 
     def sum(self):
-        # sum the cost of the vacation package here
-        if not self.myclass.validThis(
-                self.dist) or not self.passanger.validNumber() or not self.total_time.is_valid_total_time():
+        if not self.myclass.valid_this(self.dist) or not self.passenger.valid_number() or not self.total_time.is_valid_total_time():
             return -1
 
-        if(self.total_time<7):
-            number_total += 200
-
-        if(self.total_time>30 and self.passanger==2):
-            number_total -= 200
-
-
-        number_total = self.costBas
-        number_total += self.myclass.get_extraCost(self.dist)
+        number_total = self.base_cost
+        number_total += self.myclass.get_extracost(self.dist)
         number_total += self.total_time.get_fee()
         number_total -= self.total_time.get_bestpromo()
 
-        discount = self.passanger.forHereDiscount()
+        if(self.total_time.dur<7):
+            number_total += 200
+
+        if(self.total_time.dur>30 and self.passenger.num==2):
+            number_total -= 200
+
+        discount = self.passenger.for_here_discount()
         number_total = number_total - (number_total * discount)
 
-        if(self.passanger >=80):
+        if(self.passenger.num >= 80):
             return -1
         
         return max(int(number_total), 0)
@@ -106,11 +75,11 @@ class Vacation:
 
 
 def main():
-    dist = "Paris"
+    destiny = 'Paris'
     num = 5
     dur = 10
 
-    calculator = Vacation(dist, num, dur)
+    calculator = Vacation(destiny, num, dur)
     cost = calculator.sum()
 
     if cost == -1:
